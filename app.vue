b23d3ce5dfc0c9a394cdbbd1cb7856485df2f4ae
Roanna David <roannadavid@gmail.com>
2024-10-05 18:46:18 +0800

    Setup packages

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="font-main text-3xl font-bold underline">
+    Hello world!
+  </h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  modules: ['@pinia/nuxt', '@nuxt/fonts', '@nuxt/image'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  fonts: {
+    families: [
+      { name: 'Pangolin', provider: 'google' },
+      { name: 'Open Sans', provider: 'google' },
+    ]
+  },
 })
