3b6dcc3b25d3c80198a18cbb1c5bc35fae85f06e
Roanna David <roannadavid@gmail.com>
2024-10-05 19:56:02 +0800

    Add assets and main page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="text-white min-w-[20em] max-w-[30em] h-screen m-auto text-center">
+    <div class="bg-gamebg min-w-[20em] max-w-[30em] h-screen m-auto p-6">
       <slot />
     </div>
   </template>
--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="text-white">
         GALLERY
     </div>
 </template>
